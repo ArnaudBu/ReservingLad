7a80793e40a540f3e2bc0898621a6d5a8b804943
Arnaud BUZZI <abuzzi@SIA-Conseil.local>
2018-07-07 20:30:04 +0200

    Ajout de la fonction latest

---
+++ R/latest.R
@@ -0,0 +1,22 @@
+#' Latest Observations
+#'
+#' \code{latest} extracts the latests observations (payments) from a triangle.
+#'
+#' @param triangle Cumulated triangle as a matrix
+#' @return The latest observation
+#'
+#' @examples  l <- latest(triangleExampleEngland)
+#'
+#' @export
+latest <- function(triangle){
+
+  # Validity of triangle
+  if(!(is.matrix(triangle) & is.numeric(triangle))){stop("triangle is not a numeric matrix")}
+
+  # Extract the latest payments
+  l <- rev(diag(triangle[nrow(triangle):1,]))
+  names(l) <- rownames(a)
+
+  # Return the payments
+  return(l)
+}
